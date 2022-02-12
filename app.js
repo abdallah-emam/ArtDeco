0e80b7f3ac42c2ef74e46cb57f2fa61555d8597b
Abdallah Hamdy <abdo.aa797@gmail.com>
2022-02-12 17:42:19 +0200

    add intial folders

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const morgan = require('morgan');
+
+const app = express();
+
+// Development logging
+if (process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+  }
+  
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config({ path: "./config.env" });
+const app = require("./app");
+const DB = process.env.DATABASE;
+
+
+
+
+
+mongoose
+  .connect(DB)
+  .then(() => console.log("DB connection successful!"))
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
+
+
