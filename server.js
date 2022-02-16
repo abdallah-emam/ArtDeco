ef57e62053304ef5cb18dfd77b1e11a154aa8ad2
mostafaElsayed <mostafa.elsayed3537@gmail.com>
2022-02-16 18:35:52 +0200

    adding eslint

--- server.js
+++ server.js
@@ -5,17 +5,9 @@ dotenv.config({ path: "./config.env" });
 const app = require("./app");
 const DB = process.env.DATABASE;
 
-
-
-
-
-mongoose
-  .connect(DB)
-  .then(() => console.log("DB connection successful!"))
+mongoose.connect(DB).then(() => console.log("DB connection successful!"));
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
-
-
