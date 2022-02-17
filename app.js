6e9d2b897a9d8dadfa26028ed9300003b8d858d4
Abdallah <abdo.aa797@gmail.com>
2022-02-17 11:26:00 +0200

    add eslint to enhance code writting

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const morgan = require('morgan');
 const app = express();
 
 // Development logging
+
 if (process.env.NODE_ENV === 'development') {
-    app.use(morgan('dev'));
-  }
-  
+  app.use(morgan('dev'));
+}
+
 module.exports = app;
