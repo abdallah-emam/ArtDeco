2a03c3e4b34bb7bf89ec99cb88510eb69af8d659
Abdallah <abdo.aa797@gmail.com>
2022-02-20 17:25:31 +0200

    fix error in duplicate key

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -5,8 +5,9 @@ const handleCastErrorDB = (err) => {
   return new AppError(message, 400);
 };
 const handleDuplicateFieldsDB = (err) => {
-  const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
-  console.log(value);
+  // const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+  const { keyValue } = err;
+  const value = Object.values(keyValue)[0];
 
   const message = `Duplicate field value: ${value}. Please use another value!`;
   return new AppError(message, 400);
@@ -66,7 +67,7 @@ module.exports = (err, req, res, next) => {
   } else if (process.env.NODE_ENV === 'production') {
     // eslint-disable-next-line node/no-unsupported-features/es-syntax
     let error = { ...err };
-
+    console.log(error);
     if (error.name === 'CastError') error = handleCastErrorDB(error);
     if (error.code === 11000) error = handleDuplicateFieldsDB(error);
     if (error.name === 'ValidationError')
