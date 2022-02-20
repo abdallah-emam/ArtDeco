0a74359e50d02394c7982c3ec3e1a0f2a54db0ef
Abdallah <abdo.aa797@gmail.com>
2022-02-20 19:59:00 +0200

    add cross package

--- app.js
+++ app.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const morgan = require('morgan');
+const cors = require('cors');
 const globalErrorHandler = require('./controllers/errorController');
 const AppError = require('./utils/appError');
 
 const userRouter = require('./routes/userRoutes');
 const contractorRouter = require('./routes/contractorRoutes');
+const applicationRouter = require('./routes/applicationRoutes');
 
 const app = express();
 
-// Development logging
+// Enable All CORS Requests
+app.use(cors());
 
+// Development logging
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
@@ -21,6 +25,7 @@ app.use(express.json());
 // 3) ROUTES
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/contractors', contractorRouter);
+app.use('/api/v1/application', applicationRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
