1db74e0aae4de6a5af7614a1d190a296270d0b59
mostafaElsayed <mostafa.elsayed3537@gmail.com>
2022-03-03 11:27:57 +0200

    contractor email

--- controllers/authContractorController.js
+++ controllers/authContractorController.js
@@ -153,7 +153,7 @@ exports.forgotPassword = catchAsync(async (req, res, next) => {
 
   // 3) Send it to user's email
   try {
-    const resetURL = `${req.protocol}://localhost:3000/user_reset/${resetToken}`;
+    const resetURL = `${req.protocol}://localhost:3000/contractor_reset/${resetToken}`;
     await new Email(contractor, resetURL).sendPasswordReset();
 
     res.status(200).json({
